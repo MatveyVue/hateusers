34481fe56130312afa8949d0a1e7b74894ba4e7d
Whsxg <shabanov.matveyka@bk.ru>
2025-03-21 22:22:01 +0300

    Add files via upload

---
+++ src/script/home.js
@@ -0,0 +1,21 @@
+import { ref } from 'vue';
+
+export const profile = ref([
+  {
+    icon: "https://i.postimg.cc/qBsh7J0M/IMG-4891.png",
+    nick: "Whsxg",
+    infd: "Web3 Scammer",
+    info: "Web3 scammer and cyber dangerous criminal who can do anything with his team",
+    infomodl: "Web3 Scammers and dangerous cybercriminals who can do anything with their team, also prosecuted for hacking and DDoS of major exchanges and companie",
+    tags: ["Scammers", "Web3", "Toxic", "School"],
+  },
+  {
+    icon: "https://i.postimg.cc/qBsh7J0M/IMG-4891.png",
+    nick: "Danya",
+    infd: "Scammer and gift",
+    info: "Steals gifts in telegram via a special script and kidacett ddos ​​balenciage",
+    tags: ["Python", "Vue", "Geniys"],
+  },
+]);
+
+export const open = ref(false);
