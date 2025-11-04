d6cfc5e418e1a44b6a83ba37be0c63b6344a36b4
Whsxg <shabanov.matveyka@bk.ru>
2025-11-04 15:44:39 +0330

    Update HelloView.vue

--- src/views/HelloView.vue
+++ src/views/HelloView.vue
@@ -4,7 +4,7 @@
 <div style="text-align: center; margin-top: 100px;">
 <img src="../components/UtyaB.gif" width="250px"><br>
 <h3 class="heeloinf">Create ankets of your<br>acquaintances and friends<br>and hate them with us</h3>
-<RouterLink to="/home">
+<RouterLink to="">
     <button class="hatebtn">Go To Hate</button>
 </RouterLink>
 </div>
