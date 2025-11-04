01df40304b66dbd89899fcd2c8ed2b00a39196e8
Whsxg <shabanov.matveyka@bk.ru>
2025-11-04 13:33:50 +0330

    Update HelloView.vue

--- src/views/HelloView.vue
+++ src/views/HelloView.vue
@@ -1,4 +1,7 @@
 <template>
+<div id="preloader">
+  <div style="margin-top: -20px;" class="loader">loading</div>
+</div>
 <div style="text-align: center; margin-top: 100px;">
 <img src="../components/UtyaB.gif" width="250px"><br>
 <h3 class="heeloinf">Create ankets of your<br>acquaintances and friends<br>and hate them with us</h3>
@@ -14,3 +17,17 @@ body {
     overflow: hidden;
 }
 </style>
+
+<script>
+window.addEventListener('load', function() {
+    // Убираем стандартное поведение при загрузке, т.к. будем использовать setTimeout
+});
+
+// Устанавливаем таймер на 10 секунд (10000 миллисекунд)
+setTimeout(function() {
+    const preloader = document.getElementById('preloader');
+    if (preloader) { // Проверяем, существует ли элемент
+        preloader.classList.add('hidden'); // Добавляем класс для скрытия
+    }
+}, 5000); // 10000 миллисекунд = 10 секунд
+</script>
